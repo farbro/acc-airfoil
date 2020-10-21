71db74f63b60a60b9983c2a245896949b0898a6b
Emma Goodwin <emma.goodwin.2250@student.uu.se>
2020-10-21 15:21:52 +0200

    update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, send_from_directory
 from celery import Celery
 import glob
 import json
@@ -7,9 +7,10 @@ import re
 import copy
 
 app = Flask(__name__)
-celery = Celery(app.name, backend='rpc://', broker='pyamqp://worker:fnurkgurk@tweet-analyzr.local:5672/twhost')
+celery = Celery(app.name, backend='rpc://', broker='pyamqp://worker:fnurkgurk@g2-airfoil-main.local:5672/twhost')
 
 mesh_files = "data/*"
+results_path = 'data/results'
 
 result_data = {} 
 results = []
@@ -25,53 +26,22 @@ def run():
     global results
     global num_tasks
 
-    result_data['total_files'] = 0
+    # Initialize result data
     result_data['proggress'] = 0
+    result_data['files'] = []
+    result_data['total_jobs'] = 0
 
-    files = glob.glob(mesh_files)
-    num_tasks = len(files)
+    # Get parameters
+    min_angle = int(request.form['min_angle'])
+    max_angle = int(request.form['max_angle'])
+    num_angles = int(request.form['num_angles'])
+
+    num_tasks = num_angles
 
     with app.app_context():
 
-        for filepath in files:
-            results.append(process_file.delay(filepath))
+        # Push all angles to the queue
 
-    return ('', 204)
-
-@app.route("/stop", methods=["POST"])
-def stop():
-    global results
-    for result in results:
-        result.revoke()
-
-    results = []
-
-    return ('', 204)
-
-@app.route('/data', methods=['GET'])
-def get_result_data():
-    global results
-    global num_tasks
-
-    for result in results:
-        if result.ready():
-            # TODO process results and append to result_data
-            data = 0
-
-
-    result_data['proggress'] = (1 - len(results)/num_tasks)*100
-    return jsonify(result_data)
-
-@celery.task
-def process_file(filepath):
-
-    # TODO run process file with airfoil and return result
-    
-    #./airfoil  10 0.0001 10. 1 ../cloudnaca/msh/r2a15n200.xml
-
-    return something
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80 ,debug=True)
+        # Calculate angle step
+        angle_step = (max_angle - min_angle) / (num_angles-1)
 
